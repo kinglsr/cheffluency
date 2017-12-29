931902072c7b9b4cd86a9d513a10ced3d37ddfb0
kinglsr <root@sairam932.mylabserver.com>
2017-12-29 00:33:22 +0000

    apache version 0.1.1

--- cookbooks/git/metadata.rb
+++ cookbooks/git/metadata.rb
@@ -1,9 +1,9 @@
-name 'apache'
-maintainer 'The Sairam'
-maintainer_email 'saiphp123@gmail.com'
+name 'git'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
 license 'All Rights Reserved'
-description 'Installs/Configures apache'
-long_description 'Installs/Configures apache'
+description 'Installs/Configures git'
+long_description 'Installs/Configures git'
 version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
@@ -11,10 +11,10 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/apache/issues'
+# issues_url 'https://github.com/<insert_org_here>/git/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/apache'
+# source_url 'https://github.com/<insert_org_here>/git'
---
+++ cookbooks/git/recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook:: git
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+include_recipe 'git::gitinstall'
